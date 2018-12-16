968a7596411f495668845975d74dab014afab7e4
Kertich <kipsangkertich@gmail.com>
2018-12-16 19:00:49 +0300

    obedhezron

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hell_world():
-	return "hello world"
+	return render_template("home.html")
 
 
 
-if __name__ = '__main__'
-	app.run()
+if __name__ == '__main__':
+	app.run(debug = True)
